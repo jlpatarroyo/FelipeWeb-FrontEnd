88822540e15144601dc3bfba3f54c454e5423666
Jose <jl.patarroyo@uniandes.edu.co>
2020-11-02 18:19:54 -0500

    UPDATE project

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Project, ProjectService } from '../services/project.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import * as moment from 'moment';
@@ -13,79 +12,95 @@ export class ProjectComponent implements OnInit {
   constructor(private projectService: ProjectService) {}
 
   projects: Project[];
+  editingIndex:number;
 
-  editing: Boolean;
+  creating: Boolean;
+  updating: Boolean;
 
   projectForm = new FormGroup({
-    name: new FormControl('',[
+    name: new FormControl('', [
       Validators.required,
       Validators.minLength(0),
-      Validators.maxLength(100)
+      Validators.maxLength(100),
     ]),
-    description: new FormControl('',[
+    description: new FormControl('', [
       Validators.required,
       Validators.minLength(1),
-      Validators.maxLength(5000)
+      Validators.maxLength(5000),
     ]),
-    date: new FormControl('',[
-      Validators.required
-    ])
+    date: new FormControl('', [Validators.required]),
   });
 
-  onSubmit() {
-    const result = this.projectForm.value;
-    const project = {
-      name: result.name,
-      description: result.description,
-      date: result.date
-    };
-    this.projectService.postProject(project)
-    .subscribe(() => {
-      this.getProjects();
-    })
-  }
 
   ngOnInit(): void {
     this.getProjects();
-    this.editing = false;
+    this.creating = false;
+    this.updating = false;
   }
 
-  private getProjects(){
-    this.projectService.getAllProjects()
-    .subscribe(res => {
-      this.projects = res;
-    })
-  }
-
-  setEditing(p_editing: Boolean) {
-    this.editing = p_editing;
-  }
-
-  cancelEditing():void{
-    this.projectForm.reset();
-    this.setEditing(false);
-  }
-
-  validateDate():Boolean{
-    var rta = true;
-    const input_date=this.projectForm.value.date;
-    var formatted_date = moment(input_date, 'YYYY-MM-DD');
-    if(!formatted_date){
-      rta = false;
+  onProjectSubmit() {
+    const result = this.projectForm.value;
+    const new_project = {
+      name: result.name,
+      description: result.description,
+      date: result.date,
+    };
+    if(this.creating){
+      console.log("Creating");
+      this.projectService.postProject(new_project).subscribe(() => {
+        this.getProjects();
+        this.cancel();
+      });
+    }else if(this.updating){
+      console.log("Updating");
+      const curr_project = this.projects[this.editingIndex];
+      this.projectService.updateProject(curr_project.name, new_project)
+      .subscribe(() => {
+        this.getProjects();
+        this.cancel();
+      });
     }
-    return rta;
   }
 
-  getToday():String{
+  private getProjects() {
+    this.projectService.getAllProjects().subscribe((res) => {
+      this.projects = res;
+    });
+  }
+
+  setCreating() {
+    this.creating = true;
+    this.updating = false;
+  }
+
+  cancel(): void {
+    this.projectForm.reset();
+    this.creating = false;
+    this.updating = false;
+    this.editingIndex = -1;
+  }
+
+  getToday(): String {
     return new Date().toISOString().split('T')[0];
   }
 
-  removeProject(index:number){
+  removeProject(index: number) {
     const project = this.projects[index];
-    this.projectService.deleteProject(project.name)
-    .subscribe(result => {
-      console.log("Deleting project: " + result);
-      this.projects.splice(index,1);
+    this.projectService.deleteProject(project.name).subscribe(() => {
+      this.projects.splice(index, 1);
+    });
+  }
+
+  setUpdating(index:number) {
+    const curr_project = this.projects[index];
+    const curr_date:Date = moment(curr_project.date).toDate();
+    this.updating = true;
+    this.creating = false;
+    this.editingIndex = index;  
+    this.projectForm.setValue({
+      name: curr_project.name,
+      description: curr_project.description,
+      date: curr_date.toISOString().split('T')[0]
     });
   }
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,34 +2,36 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
-export interface Project{
-    id:String,
-    name:String,
-    description:String,
-    date:Date
+export interface Project {
+  id: String;
+  name: String;
+  description: String;
+  date: Date;
 }
 
 @Injectable()
 export class ProjectService {
-    constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-    base_url = "http://localhost:8000/projects";
+  base_url = 'http://localhost:8000/projects';
 
-    getAllProjects(): Observable<Project[]>{
-        return this.http.get<Project[]>
-        (this.base_url)
-    }
+  getAllProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.base_url);
+  }
 
-    getProject(name:String): Observable<Project>{
-        return this.http.get<Project>
-        (this.base_url + '/' + name)
-    }
+  getProject(name: String): Observable<Project> {
+    return this.http.get<Project>(this.base_url + '/' + name);
+  }
 
-    postProject(project:{}):Observable<Project>{
-        return this.http.post<Project>(this.base_url, project);
-    }
+  postProject(project: {}): Observable<Project> {
+    return this.http.post<Project>(this.base_url, project);
+  }
 
-    deleteProject(name:String):Observable<Project>{
-        return this.http.delete<Project>(this.base_url + '/' + name);
-    }
+  deleteProject(name: String): Observable<Project> {
+    return this.http.delete<Project>(this.base_url + '/' + name);
+  }
+
+  updateProject(curr_name: String, new_project: {}) {
+    return this.http.put<Project>(this.base_url + '/' + curr_name, new_project);
+  }
 }
